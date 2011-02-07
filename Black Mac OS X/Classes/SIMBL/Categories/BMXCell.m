82a53a6ca1f2a91c0758a7437cccc9862b67b659
Alex Zielenski <alex@zielenski.net>
2011-02-07 06:07:10 -0500

    fixed unreadable text issue

--- Black Mac OS X/Classes/SIMBL/Categories/BMXCell.m
+++ Black Mac OS X/Classes/SIMBL/Categories/BMXCell.m
@@ -7,7 +7,7 @@
 //
 
 #import "BMXCell.h"
-
+#import "NSThemeFrame.h"
 
 @implementation NSCell (BMXCell)
 + (void)swizzle {
@@ -28,9 +28,20 @@
 }
 #pragma mark - Overrides
 - (NSBackgroundStyle)new_backgroundStyle {
-//	if (self.controlView.superview.isBMXCustomized == YES)
-//		return NSBackgroundStyleLowered;
-	return [self orig_backgroundStyle]; // this stuff doesn't work. any help?
+	/*if (self.controlView.superview.isBMXCustomized == YES)
+		return NSBackgroundStyleLowered;
+	return NSBackgroundStyleLight; // this stuff doesn't work. any help?*/
+	if (self.controlView) {
+		NSThemeFrame *themeFrame = (NSThemeFrame*)[[[self.controlView window] contentView] superview];
+		NSRect topBar = NSMakeRect(0, NSMaxY(themeFrame.frame)-themeFrame._topBarHeight, themeFrame.frame.size.width, themeFrame._topBarHeight);
+		NSRect bottomBar = NSMakeRect(0, 0, themeFrame.frame.size.width, themeFrame._bottomBarHeight);
+		NSRect frame = self.controlView.frame;
+		frame = [self.controlView convertRect:frame toView:nil];
+
+		if (NSEqualRects(NSIntersectionRect(topBar, frame), frame)||NSEqualRects(NSIntersectionRect(bottomBar, frame), frame))
+			return NSBackgroundStyleLowered; // If the controlVIew's entire frame is inside the top bar or the bottom bar, make the cell text/images white. Coincidentally works for toolbars as well
+	}
+	return [self orig_backgroundStyle];
 }
 //- (NSBackgroundStyle)interiorBackgroundStyle {
 //	return NSBackgroundStyleLowered;
